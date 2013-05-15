f06c0a2191d248df34c820c9390f1a3501173ac6
klaus.hauschild.1984@gmail.com <klaus.hauschild.1984@gmail.com@516da800-371c-905f-11dc-4a23c38cc680>
2013-05-15 18:07:07 +0000

    fix Issue 1: Player kann das Spielfeld verlassen

--- trunk/dodge/dodge-core/src/main/java/de/hauschild/dodge/core/game/Enemy.java
+++ trunk/dodge/dodge-core/src/main/java/de/hauschild/dodge/core/game/Enemy.java
@@ -10,27 +10,4 @@ public class Enemy extends Actor {
         * PlayN.graphics().height()), new Point(PlayN.random() - 0.5f, PlayN.random() - 0.5f).normalized().multiply(5f));
   }
 
-  @Override
-  public void update(final int delta) {
-    final float x = bounce(getPosition().getX(), PlayN.graphics().width(), getVelocity().getX());
-    final float y = bounce(getPosition().getY(), PlayN.graphics().height(), getVelocity().getY());
-    if (x != 0f || y != 0f) {
-      if (y == 0f) {
-        setVelocity(new Point(x, getVelocity().getY()));
-      } else if (x == 0f) {
-        setVelocity(new Point(getVelocity().getX(), y));
-      } else {
-        setVelocity(new Point(x, y));
-      }
-    }
-    super.update(delta);
-  }
-
-  private float bounce(final float value, final float max, final float velocity) {
-    if (value < 0f || value > max) {
-      return -velocity;
-    }
-    return 0f;
-  }
-
 }
