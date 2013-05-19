504bc6b6c80b12f6373c1bb92d2a8c39d2a8dee7
nice2000.de@googlemail.com <nice2000.de@googlemail.com@516da800-371c-905f-11dc-4a23c38cc680>
2013-05-19 10:01:53 +0000

    - improve rotation and scale - set origin of the text to midle

--- dodge/dodge-core/src/main/java/de/hauschild/dodge/core/welcome/WelcomeGameState.java
+++ dodge/dodge-core/src/main/java/de/hauschild/dodge/core/welcome/WelcomeGameState.java
@@ -11,18 +11,21 @@ import de.hauschild.dodge.core.state.GameStateController;
 
 public class WelcomeGameState extends AbstractGameState {
 
+  private static int DODGE_TEXT_SCALE = 60;
   private GroupLayer rootLayer;
   private boolean goToNextState;
   private boolean startFadeOut;
-  private int angle = 0;
-  private int scale = 0;
+  private float angle = 0;
+  private float scale = 1;
+  private float smoothAngle;
+  private float smoothScale;
 
   @Override
   public void init(final GameStateController gameStateController) {
     super.init(gameStateController);
     rootLayer = PlayN.graphics().rootLayer();
     Utils.addBackGround(rootLayer, Color.rgb(255, 255, 255));
-    Utils.addMessageText(rootLayer, 200, 200, "dodge", 60, Color.rgb(255, 0, 0));
+    Utils.addMessageText(rootLayer, 200, 200, "dodge", DODGE_TEXT_SCALE, Color.rgb(255, 0, 0));
     Utils.addMessageText(rootLayer, 500, 460, "@2013 Klaus Hauschild", 12, Color.rgb(0, 0, 0));
     PlayN.pointer().setListener(new Listener() {
 
@@ -48,22 +51,25 @@ public class WelcomeGameState extends AbstractGameState {
 
   @Override
   public void paint(final float alpha) {
+    // TODO: get better x value from text to set middle
+    rootLayer.get(1).setOrigin(70, DODGE_TEXT_SCALE / 2);
+    rootLayer.get(1).setRotation(smoothAngle);
 
+    if (startFadeOut && smoothScale > 1) {
+      rootLayer.get(1).setScale((smoothScale));
+    }
   }
 
   @Override
   public void update(final int delta) {
     angle++;
-    final float smoothRotateAngel = (float) (angle * -(delta * .25));
-    rootLayer.get(1).setRotation(smoothRotateAngel);
-    if (smoothRotateAngel > 360) {
-      angle = 0;
-    }
+    smoothAngle = angle / delta;
 
     if (startFadeOut) {
       scale++;
-      rootLayer.get(1).setScale((float) (scale / -(delta * .75)));
-      if (scale == 100) {
+      smoothScale = scale / (delta * .75f);
+
+      if (scale == 150) {
         goToNextState = true;
       }
     }
